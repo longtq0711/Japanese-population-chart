7791d3c8da88429cf9b2b52da5c0a06287a965aa
Long Tran Quang <longtq0711@gmail.com>
2021-09-22 23:48:24 +0700

    Base project + Test get data from RESAS

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,45 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>都道府県</h1>
+    <label v-for="(item, key) in prefectures" :key="key">
+      <input type="checkbox">
+      {{ item.prefName }}
+    </label>
+    <PopulationChart></PopulationChart>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
+import PopulationChart from "./components/PopulationChart.vue";
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    PopulationChart
+  },
+  data() {
+    return {
+      prefectures: []
+    }
+  },
+  async created() {
+    const APIKEY = "aOxiRQPVhQ3Qxzs55UWDKo60M51tRnEY46QyIEm1";
+    const headers = {
+      'X-API-KEY':APIKEY
+    }
+    let scop = this;
+    axios({
+      method: 'GET',
+      url: 'https://opendata.resas-portal.go.jp/api/v1/prefectures',
+      headers: headers
+    }).then(response => {
+      let data = response.data;
+      scop.prefectures = data.result;
+    }).catch(error => {
+      console.log(error.message);
+    })
+
   }
 }
 </script>
---
+++ src/components/PopulationChart.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <line-chart 
+      :chart-data= datacollection
+    ></line-chart>
+  </div>
+</template>
+
+<script>
+  import LineChart from './PopulationChart.js'
+
+  export default {
+    components: {
+      LineChart
+    },
+    data () {
+      return {
+        datacollection: null,
+        loaded: false,
+        temperature: [0,1,2,3,4,5,6,7,8,9],
+        humidity: [9,8,7,6,5,4,3,2,1,0],
+        potentio: [4,5,4,5,4,5,4,5,4,5],
+        time: [0,1,2,3,4,5,6,7,8,9]
+      }
+    },
+    async mounted () {
+      this.fillData();
+      console.log(this.datacollection);
+    },
+    methods: {
+      fillData () {
+        this.datacollection = {
+          labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+          datasets: [
+            {
+              label: 'Temp (°C)',
+              backgroundColor: 'rgba(0, 0, 255, 0.2)',
+              borderColor: 'lightblue', 
+              pointBackgroundColor: 'blue', 
+              borderWidth: 1, 
+              pointBorderColor: 'blue',
+              data: [1,2,3,2,1,3,4,6,7,8]
+              // data: this.temperature
+            }, 
+          ]
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  .small {
+    width: 500px;
+    height: 100px;
+    margin: auto
+  }
+</style>
